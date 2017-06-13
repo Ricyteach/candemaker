3d13c1a567459d91d4f4d5ee9ac20b824ce191b5
Rick Teachey <ricky@teachey.org>
2017-06-13 15:50:54 -0400

    changes to blank module
    Added BlankFloat

--- src/candemaker/cid_parmatters/blank.py
+++ src/candemaker/cid_parmatters/blank.py
@@ -1,11 +1,26 @@
-from string import Formatter
+class Blank():
+    def __new__(cls, value=0):
+        try:
+            obj = super().__new__(cls, value)
+            return obj
+        except ValueError:
+            if value == '' or value == ' ':
+                return super().__new__(cls, 0.0)
+            else:
+                raise
+    def __str__(self):
+        return '' if self==0 else super().__str__()
+    def __format__(self, spec):
+        if (spec.endswith('d') or spec.endswith('f') or spec.endswith('n')) and self==0:
+            spec = spec[:-1]+'s'
+            return format('',spec)
+        else:
+            return super().__format__(spec)
 
-class BlankInt(int):
-    '''An int that prints a blank when zero.'''
-    def __str__(self):
-        return '' if self==0 else super().__str__()
+class BlankInt(Blank, int):
+    '''An int that prints blank when zero.'''
+    pass
         
-class BlankFloat(float):
-    '''A float that prints a blank when zero.'''
-    def __str__(self):
-        return '' if self==0 else super().__str__()
+class BlankFloat(Blank, float):
+    '''A float that prints blank when zero.'''
+    pass
